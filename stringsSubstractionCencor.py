40ead7f84275beeaaa505984409b706761f2252f
Rafael Shamatov <117221723+khovansky007@users.noreply.github.com>
2022-12-15 17:16:51 +0500

    Update stringsSubstractionCencor.py

--- stringsSubstractionCencor.py
+++ stringsSubstractionCencor.py
@@ -2,30 +2,70 @@
 class Deleting_string:
     @staticmethod
     def delete(text: str, minus: str) -> str:
-        new = []
+        
         n = 0
 
-        for i in text:
-            for x in minus[n]:
-                if i != x:
-                    new.append(i)
-                    n -= 1
+        startSpace = 0
+        countSpace = text.count(' ')
+        listWordsText = [] # [str]
+        for d in range(1, countSpace + 1 + 1):
+            Flag = True
+            while Flag == True:
+                word = []
+                for i in text:
+
+                    if i != ' ':
+                        word.append(i)
+                    else:
+                        Flag = False
+                        break
+                if d == countSpace + 1:
+                    Flag = False
                     break
-                elif i == x and minus in text:
-                    break
+                
+            listWordsText.append(''.join(word))
+            text = text[len(word):]  # - (' ' + word)
+            try:
+                if text[0] == ' ':
+                    text = text[1:]
+            except:
+                ...
+
+       
+        n = 0
+        cencuredPhrase = []
+        for word in listWordsText:
+            cencuredWord = []
+            for i in word:
+                for x in minus[n]:
+                    if i != x:
+                        cencuredWord.append(i)
+                        n -= 1
+                        break
+                    elif i == x and minus in word:
+                        break
+                    else:
+                        cencuredWord.append(i)
+                        n -= 1
+                        break
+                if n < len(minus)-1:
+                    n += 1
                 else:
-                    new.append(i)
-                    n -= 1
-                    break
-            if n < len(minus)-1:
-                n += 1
-            else:
-                n = 0
+                    n = 0
+
+            cencuredPhrase.append(''.join(cencuredWord))
+
+        responce = ''
+        for i in range(len(cencuredPhrase)):
+            responce += cencuredPhrase[i] + ' '
+        responce = responce[:-1]
+
+
         
 
-        return (''.join(new))
-    
-    
+        return responce
+
+        
     # -------TESTs---------
 
     # assert delete('spiderman', 'spi') == 'derman'
